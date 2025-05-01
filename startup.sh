c92953d18509874ace2aecd61ec050d9066be296
Mutasem Alhami <mutasem.alhami@nl.ey.com>
2025-05-01 16:41:02 +0200

    new changes

--- startup.sh
+++ startup.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# Install necessary dependencies
-apt-get update && apt-get install -y libgl1-mesa-glx
-
-# Run the Streamlit app
-streamlit run vehicledamage2.py --server.port=$PORT --server.address=0.0.0.0
+streamlit run vehicledamage2.py --server.port 8000 --server.address 0.0.0.0
